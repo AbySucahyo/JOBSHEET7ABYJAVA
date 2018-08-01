79363b4e1d2dd86cb77e8a1be2e67d4cc94ab13a
Aby <Aby@LAPTOP-4FNLA4QA>
2018-07-31 19:23:08 -0700

    Initial Commit

---
+++ src/Latihan/CobaPercabangan1.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author Aby
+ */
+public class CobaPercabangan1 {
+    String identitas = "Aby Sucahyo / XRPL5 / 01";
+    System.out.println("identitas : "+ identitas);
+}
