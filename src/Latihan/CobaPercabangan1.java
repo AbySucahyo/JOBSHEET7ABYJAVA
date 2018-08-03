230f1d2aaff823078d4dc8e658aa1a4aa1599b44
Aby <Aby@LAPTOP-4FNLA4QA>
2018-08-02 17:46:49 -0700

    Coba IF

--- src/Latihan/CobaPercabangan1.java
+++ src/Latihan/CobaPercabangan1.java
@@ -5,15 +5,39 @@
  */
 package Latihan;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Aby
  */
 public class CobaPercabangan1 {
+
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         
     String identitas = "Aby Sucahyo / XRPL5 / 01";
     System.out.println("identitas : "+ identitas);
- 
+    
+  Scanner scanner = new Scanner(System.in);
+System.out.print("Nilai a ? ");
+int a = scanner.nextInt();
+
+System.out.println("\nIF.. 1");
+if(a < 5) System.out.println("Nilai a kurang dari 5");
+
+System.out.println("\nIF.. 2");
+if(a == 5) System.out.println("Nilai a sama dengan 5");
+
+System.out.println("\nIF.. 3");
+if(a > 5) {
+    System.out.println("isi variabel a : "+ a);
+    System.out.println("Nilai a lebih dari 5 ");
+}
         }
+    
+    
 }
