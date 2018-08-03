58d3f786d782ca40ba41dada001e1090d495398b
Aby <Aby@LAPTOP-4FNLA4QA>
2018-08-02 20:35:54 -0700

    Coba IF..ELSE IF..ELSE

--- src/Latihan/CobaPercabangan1.java
+++ src/Latihan/CobaPercabangan1.java
@@ -43,5 +43,26 @@ if (a < 5){
     System.out.println("Nilai kurang dari 5");
 } else System.out.println("Nilai lebih dari atau sama dengan 5");
 
+System.out.println("\nIF..ELSE IF..ELSE.. 1");
+if (a < 5) {
+    System.out.println("Nilai kurang dari 5");
+} else if (a == 5) {
+    System.out.println("Nilai a sama dengan 5");
+} else 
+    System.out.println("Nilai a lebih dari 5");
+
+System.out.println("\nIF..ELSE IF..ELSE.. 2");
+if (a < 2) {
+    System.out.println("Nilai a kurang dari 2");
+} else if (a < 4) {
+    System.out.println("Nilai a kurang dari 4");
+} else if (a < 6) {
+    System.out.println("Nilai a kurang dari 6");
+} else if (a == 6) {
+    System.out.println("Nilai a sama dengan 6");
+} else {
+    System.out.println("Nilai a lebih dari 6");
+}
+
         }
       }
