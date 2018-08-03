5dfd35887b2de8335cac0f7e4a8bc3380b3fedc9
Aby <Aby@LAPTOP-4FNLA4QA>
2018-08-02 21:03:59 -0700

    Initial Commit

---
+++ src/Latihan/SaranBahan2Resep.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author Aby
+ */
+public class SaranBahan2Resep {
+    public static void main(String[] args) {
+        String identitas = "Aby Sucahyo / XRPL 5 / 01";
+        System.out.println("Identitas : " + identitas );
+        
+         System.out.println("\nSaran Resep dari Bahan Milik Anda\n");
+         
+    }
+}
